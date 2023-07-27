2afc47c866498f1fff49c584e5994b0c282a84ec
Adam Polyak cygwin <adam.polyak.email+cygwin@gmail.com>
2023-07-27 15:58:10 +0100

    init spring framework

---
+++ src/main/java/com/dbx/word_string_puzzle/WordStringPuzzleApplication.java
@@ -0,0 +1,13 @@
+package com.dbx.word_string_puzzle;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class WordStringPuzzleApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(WordStringPuzzleApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/dbx/word_string_puzzle/WordStringPuzzleApplicationTests.java
@@ -0,0 +1,13 @@
+package com.dbx.word_string_puzzle;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class WordStringPuzzleApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
