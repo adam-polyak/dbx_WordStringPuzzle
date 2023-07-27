1a2625a0d59a6172764f5067bbc85d68e054c02f
Ádám Polyák <adam.polyak.emal@gmail.com>
2023-07-27 18:01:05 +0200

    add input scanner function

--- src/main/java/com/dbx/word_string_puzzle/WordStringPuzzleApplication.java
+++ src/main/java/com/dbx/word_string_puzzle/WordStringPuzzleApplication.java
@@ -3,11 +3,35 @@ package com.dbx.word_string_puzzle;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 
+import java.util.ArrayList;
+import java.util.Arrays;
+import java.util.List;
+import java.util.Scanner;
+
 @SpringBootApplication
 public class WordStringPuzzleApplication {
+    public static void main(String[] args) {
+        SpringApplication.run(WordStringPuzzleApplication.class, args);
 
-	public static void main(String[] args) {
-		SpringApplication.run(WordStringPuzzleApplication.class, args);
-	}
+        List<String> inputWordList = obtainInputWords();
 
+        //buildWordStringGraph(inputWordList);
+
+    }
+
+    private static List<String> obtainInputWords() {
+        Scanner scanner = new Scanner(System.in);
+
+        System.out.println("Please provide the word list separated by one space character: ");
+        String scannedLine = scanner.nextLine();
+        //System.out.println("Scanned line: " + scannedLine);
+
+        List<String> wordList = new ArrayList<>(Arrays.asList(scannedLine.split("\\s")));
+        System.out.println("Scanned words (" + wordList.size() + " piece(s)):");
+        for(String word : wordList) {
+            System.out.println(word);
+        }
+
+        return wordList;
+    }
 }
