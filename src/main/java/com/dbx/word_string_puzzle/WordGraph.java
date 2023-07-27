d9faf8f9e2fba415418ae22fb7f15a50c5d362ee
Ádám Polyák <adam.polyak.emal@gmail.com>
2023-07-27 21:33:22 +0200

    add Hamilton path seeker algorithm

--- src/main/java/com/dbx/word_string_puzzle/WordGraph.java
+++ src/main/java/com/dbx/word_string_puzzle/WordGraph.java
@@ -1,11 +1,155 @@
 package com.dbx.word_string_puzzle;
 
+import java.util.ArrayList;
+import java.util.Arrays;
+import java.util.List;
+import java.util.Objects;
+
+
+/**
+ * A class to store a graph edge
+ */
+class Edge
+{
+    int source, dest;
+
+    public Edge(int source, int dest)
+    {
+        this.source = source;
+        this.dest = dest;
+    }
+}
+
+/**
+ * A class to represent a graph object
+ */
+class Graph
+{
+    // A list of lists to represent an adjacency list
+    List<List<Integer>> adjList = null;
+
+    // Constructor
+    Graph(List<Edge> edges, int n)
+    {
+        adjList = new ArrayList<>();
+
+        for (int i = 0; i < n; i++) {
+            adjList.add(new ArrayList<>());
+        }
+
+        // add edges to the undirected graph
+        for (Edge edge: edges)
+        {
+            int src = edge.source;
+            int dest = edge.dest;
+
+
+//            adjList.get(getElementByName(adjList, src)).add(dest);
+//            adjList.get(getElementByName(adjList, dest)).add(src);
+
+            adjList.get(src).add(dest);
+            adjList.get(dest).add(src);
+        }
+    }
+
+    /*private ArrayList<Integer> getElementByName(List<List<Integer>> adjList, String name) {
+        for(int i = 0; i < adjList.size(); i++) {
+            if (((String)adjList.get(i)).equals(name)) {
+                return i;
+            }
+        }
+    }*/
+}
+
+
 /**
  * Graph related classes and methods<br>
  * @author Ádám Polyák <adam.polyak.email at gmail.com>
  * */
 public class WordGraph {
     private static final Boolean debugPrint = false;
+
+
+
+
+    public static void hamiltonianPaths(Graph graph, int v, boolean[] visited,
+                                        List<Integer> path, int n)
+    {
+        // if all the vertices are visited, then the Hamiltonian path exists
+        if (path.size() == n)
+        {
+            // print the Hamiltonian path
+            System.out.println(path);
+            return;
+        }
+
+        // Check if every edge starting from vertex `v` leads
+        // to a solution or not
+        for (int w: graph.adjList.get(v))
+        {
+            // process only unvisited vertices as the Hamiltonian
+            // path visit each vertex exactly once
+            if (!visited[w])
+            {
+                visited[w] = true;
+                path.add(w);
+
+                // check if adding vertex `w` to the path leads
+                // to the solution or not
+                hamiltonianPaths(graph, w, visited, path, n);
+
+                // backtrack
+                visited[w] = false;
+                path.remove(path.size() - 1);
+            }
+        }
+    }
+
+    public static void findHamiltonianPaths(Graph graph, int n)
+    {
+        // start with every node
+        for (int start = 0; start < n; start++)
+        {
+            // add starting node to the path
+            List<Integer> path = new ArrayList<>();
+            path.add(start);
+
+            // mark the start node as visited
+            boolean[] visited = new boolean[n];
+            visited[start] = true;
+
+            hamiltonianPaths(graph, start, visited, path, n);
+        }
+    }
+
+    public static void mainn(List<String> args)
+    {
+        List<Edge> edges = new ArrayList<Edge>();
+        for (int i = 0; i < args.size(); i++) {
+            for (int j = 0; j < args.size(); j++) {
+                if(i == j) {
+                    continue;
+                }
+                boolean isAdjacent = areWordsAdjacent(args.get(i), args.get(j));
+                if (isAdjacent) {
+                    edges.add(new Edge(i, j));
+                }
+            }
+        }
+
+
+        // total number of nodes in the graph (labelled from 0 to 3)
+        int n = args.size();
+
+        // build a graph from the given edges
+        Graph graph = new Graph(edges, n);
+
+        findHamiltonianPaths(graph, n);
+    }
+
+
+
+
 
 
     /**
